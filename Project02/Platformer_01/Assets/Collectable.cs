508e93986a3f03994a7cd6c6bf525f35fc60d820
Caleb McDonald <89492661+mcd8294@users.noreply.github.com>
2023-02-20 15:48:39 -0800

    Platformer Part I Final Edits

--- Project02/Platformer_01/Assets/Collectable.cs
+++ Project02/Platformer_01/Assets/Collectable.cs
@@ -11,13 +11,11 @@ public class Collectable : MonoBehaviour
     public TextMeshProUGUI scoreBoard;
     public int coinCount = 0;
     
-    // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         Ray toMouse = gameCamera.ScreenPointToRay(Input.mousePosition);
@@ -35,5 +33,4 @@ public class Collectable : MonoBehaviour
             }
         }
     }
-    
 }
